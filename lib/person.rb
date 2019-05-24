a31ca603fb76a7926e24d13c9470b29e6a6539a2
Alex Velasquez <jtinnesco@gmail.com>
2019-05-24 18:55:08 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,10 +1,10 @@
 class Person
   
-  def name
+  def name              # getter method 
     @name
   end
  
-  def name=(new_name)
+  def name=(new_name)   # setter method 
     @name = new_name
   end
   
